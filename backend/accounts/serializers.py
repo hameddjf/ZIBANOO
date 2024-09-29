d6c82289233fab42b31843956cedb01731e64605
Devil <amyrmhmdhmzhay950@gmail.com>
2024-09-29 10:57:15 +0330

    Create accounts api

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import CustomUser
+from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import default_token_generator
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', "username", 'first_name', 'last_name', 'email', 'phone_number', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = get_user_model()(**validated_data)
+        user.set_password(validated_data['password'])
+        user.is_active = False  # فعال‌سازی حساب در آینده
+        user.save()
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+
+class CustomUserChangeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'phone_number']
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.phone_number = validated_data.get('phone_number', instance.phone_number)
+        instance.save()
+        return instance
