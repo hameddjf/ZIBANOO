e2c3a25ab499f65842a896dee9cbf023d52bba13
Devil <amyrmhmdhmzhay950@gmail.com>
2024-10-03 11:28:16 +0330

    Create products api

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,4 @@
-# views.py
+
 from rest_framework import viewsets
 from .models import Product, ProductImages, ProductVariant, Future
 from .serializers import ProductSerializer, ProductImagesSerializer, ProductVariantSerializer, FutureSerializer
