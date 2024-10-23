ddd828766c18c645b5c42d5b8f7bb2b4809268c1
hamed moradi <99538932+hameddjf@users.noreply.github.com>
2024-10-23 19:28:50 +0330

    Revert "intial frontEnd"

---
+++ Shope Front end/src/routers/AllRouters.jsx
@@ -0,0 +1,9 @@
+import Home from "../pages/Home/Home";
+
+///////////////////////////////////
+// Enter Componets Page and router
+//////////////////////////////////
+
+const AllRouters = [{ path: "/", element: <Home /> }];
+
+export default AllRouters;
